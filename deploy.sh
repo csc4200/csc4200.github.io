e4e8aed5b29f714c4b5a58c4698112f76e987cf4
sshannigrahi <susmit.shannigrahi@gmail.com>
2020-08-23 19:21:33 -0500

    Publishing to master

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo -e "Deploying updates to GitHub..."
 hugo
-git add --all && git commit -m "Publishing to master" && cd ..
+git add --all && git commit -m "Publishing to master"
 git push origin master
+
 cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
 git push origin gh-pages 
 
