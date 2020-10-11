ee934ecc61e9456bb20af61b37819fb8e54456dd
sshannigrahi <susmit.shannigrahi@gmail.com>
2020-10-11 18:55:10 -0500

    Publishing to master

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ rm -rf docs
 cp -r public/ docs/
 git add --all && git commit -m "Publishing to master"
 git push origin master
+rm -f public/recordings/*
 
 #cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
 #git push origin gh-pages 
